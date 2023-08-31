0e1d30a599b15e6ece06b5b1c6515e7f2b554a45
Leonid Korshunov <ultimatepowercoder@gmail.com>
2023-08-31 23:54:01 +0400

    Добавил gulp-file-include в gulpfile.js

--- gulp-html-scss/gulpfile.js
+++ gulp-html-scss/gulpfile.js
@@ -1,8 +1,26 @@
-const gulp = require('gulp');
+// Подключаем gulp:
+const gulp = require('gulp');  
 
-gulp.task('hello', function(done){
-    console.log('Hello from GULP!');
-    done()
-})
+// Подключаем пакет gulp file include:
+const fileInclude = require('gulp-file-include');
 
-gulp.task('default', gulp.series('hello'));
+// Пишем task обрабатывающий html файлы:
+gulp.task('includeFiles', function(){
+    // Функция возвращает поток, поэтому gulp src создает поток.
+    // Далее через "пайпы" мы работаем с файлами и сохраняем их куда нам нужно. 
+    // Мы будем обрабатывать файлы из папки src за исключением папки blocks.
+    return gulp.src('./src/*.html')
+        .pipe(fileInclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(gulp.dest('./dist/'))
+});
+
+
+// gulp.task('hello', function(done){
+//     console.log('Hello from GULP!');
+//     done()
+// })
+
+// gulp.task('default', gulp.series('hello'));
